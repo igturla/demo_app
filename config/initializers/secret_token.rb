24ccc06ab702bc87cef7714e0b919872abab0d25
Isaiah Turla <isaiahturla@glyphgames.com>
2013-06-06 15:23:33 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '6246d88d38ec2b80012c4fe2b33a63cd18b175affa9958e539659f0b87738cb2bc15d5a54e64d5ff0b32fca4d5223897728716714bb4027ef3a21d42b8664466'
